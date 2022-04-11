ee01084de2468b530b40ad447f325c9089557fa1
Mohamed Elsayed <test.deal.automation@gmail.com>
2022-04-11 19:29:48 +0300

    add create fixed contract test suite

---
+++ test/specs/createContract.js
@@ -0,0 +1,14 @@
+const LoginPage = require('../pageobjects/login.page');
+const CreateContract = require('../pageobjects/createContract.page');
+const UserData = require('../testData/user.json');
+const ContractData = require('../testData/fixedContract.json');
+
+describe ('Login and Create Fixed Contract', ()=> {
+    it ('Login with Credentials then create a fixed rate contract', async ()=> {
+        await LoginPage.open();
+        await LoginPage.login(UserData.user);
+        await browser.pause(3000);
+        await CreateContract.open();
+        await CreateContract.createFixedContract(ContractData.contract);
+    });
+});
